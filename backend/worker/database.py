5f19c6d6723c3104e985edcd0f72641d29c1c1db
supass <112678332+SupaschaiPh@users.noreply.github.com>
2024-11-06 17:51:51 +0700

    fix: worker python
    change broker & value format

--- backend/worker/database.py
+++ backend/worker/database.py
@@ -5,11 +5,11 @@ conf = "http::addr=localhost:9000;username=admin;password=quest;"
 
 
 
-def append_row(temperature,humidity):
+def append_row(device_id,temperature,humidity):
     with Sender.from_conf(conf) as sender:
         sender.row(
             'sensors',
-            symbols={'id': 'device'},
+            symbols={'id': device_id},
             columns={'temperature': temperature, 'humidity': humidity},
             at=TimestampNanos.now())
         sender.flush()
--- backend/worker/main.py
+++ backend/worker/main.py
@@ -2,21 +2,22 @@ import paho.mqtt.client as mqtt
 import database , json , pytz
 
 bkk_timezone = pytz.timezone('Asia/Bangkok')
+broker = "broker.hivemq.com"
 
 def on_connect(client, userdata, flags, reason_code, properties):
-    
     print(f"Connected with result code {reason_code}")
     client.subscribe("temp-hum-pj/send")
 
 def on_message(client, userdata, msg):
     val = json.loads(msg.payload)
-    database.append_row(val["T"], val["H"])
+    database.append_row(val["id"],val["T"], val["H"])
     print(val)
 
+print(f"Connecting")
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 
-mqttc.connect("localhost", 1883, 60)
+mqttc.connect(broker, 1883, 60)
 
 mqttc.loop_forever()

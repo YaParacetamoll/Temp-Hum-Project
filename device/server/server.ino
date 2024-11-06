49b166ece7e8bf2036c9ecb9d945890160f9a154
Pat worada <65070203@kmitl.ac.th>
2024-11-06 17:51:16 +0700

    modify server to publish mqtt & get value from client

--- device/server/server.ino
+++ device/server/server.ino
@@ -8,16 +8,20 @@ ArduinoLEDMatrix matrix;
 #include <WiFiS3.h>
 #include "arduino_secrets.h"
 
-#define DEVICE_ID 242
-#define FLAG "CSL"
-
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 
+//// Lib lora server
+#include <SPI.h>
+#include <RH_RF95.h>
+#define RFM95_CS 10
+#define RFM95_RST 7
+#define RFM95_INT 2
+#define RF95_FREQ 909.56
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-
-const char broker[] = "phycom.it.kmitl.ac.th"; //"broker.hivemq.com";
+const char broker[] = "mqtt-dashboard.com"; //"broker.hivemq.com";
 int port = 1883;
 const char send_topic[] = "temp-hum-pj/send";
 
@@ -34,10 +38,6 @@ int status = WL_IDLE_STATUS;  // the WiFi radio's status
 #include "Adafruit_SHT31.h"
 #include <LCDI2C_Multilingual.h>
 
-LCDI2C_Symbols lcd(0x27, 16, 2);  // I2C address: 0x27; Display size: 16x2
-
-bool enableHeater = false;
-uint8_t loopCnt = 0;
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
@@ -81,19 +81,6 @@ void setupNetwork() {
   Serial.print("You're connected to the network");
 }
 
-void setupTempSensor() {
-  Serial.println("SHT31 test");
-  if (!sht31.begin(0x44)) {  // Set to 0x44, 0x45 for alternate i2c addr
-    Serial.println("Couldn't find SHT31");
-    while (1) delay(1);
-  }
-
-  Serial.print("Heater Enabled State: ");
-  if (sht31.isHeaterEnabled())
-    Serial.println("ENABLED");
-  else
-    Serial.println("DISABLED");
-}
 
 void setupMQTT() {
   // attempt to connect to Wifi network:
@@ -126,71 +113,45 @@ void setup() {
 
   while (!Serial)
     delay(10);  // will pause Zero, Leonardo, etc until serial console opens
-  setupTempSensor();
-  // Setup LCD Display
-  lcd.init();
-  lcd.backlight();
   setupMQTT();
-}
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
 
-void displayToLcd(String line1, String line2) {
-  lcd.setCursor(0, 0);
-  //lcd.println("Temperature:25°C" , line1);
-  lcd.println(line1);
-  lcd.println(line2);
-}
+  while (!Serial);
+  Serial.begin(9600);
+  delay(100);
 
-struct tempAndHumStruct {
-  float temp;
-  float hum;
-};
+  Serial.println("Arduino LoRa RX Test!");
+  
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
 
-void getTempAndHum(struct tempAndHumStruct *source) {
-  float t = sht31.readTemperature();
-  float h = sht31.readHumidity();
-
-  if (!isnan(t) && !isnan(h)) {  // check if 'is not a number'
-
-    // Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
-    source->temp = t;
-    source->hum = h;
-  } else {
-    // Serial.println("Failed to read temperature");
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
   }
+  Serial.println("LoRa radio init OK!");
 
-
-  delay(1000);
-
-  // Toggle heater enabled state every 30 seconds
-  // An ~3.0 degC temperature increase can be noted when heater is enabled
-  if (loopCnt >= 30) {
-    enableHeater = !enableHeater;
-    sht31.heater(enableHeater);
-    Serial.print("Heater Enabled State: ");
-    if (sht31.isHeaterEnabled())
-      Serial.println("ENABLED");
-    else
-      Serial.println("DISABLED");
-
-    loopCnt = 0;
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
   }
-  loopCnt++;
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
 }
 
-void publishMessageMQTT(String mss) {
-  // call poll() regularly to allow the library to send MQTT keep alive which
-  // avoids being disconnected by the broker
-  mqttClient.poll();
-  unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time a message was sent
-    previousMillis = currentMillis;
-    mqttClient.beginMessage(send_topic);
-    mqttClient.print(mss);
-    mqttClient.endMessage();
-  }
-}
+
 
 void checkWifiIsConnecting() {
   if (WiFi.status() == WL_CONNECTED) {
@@ -200,20 +161,51 @@ void checkWifiIsConnecting() {
   }
 }
 
+void publishMessageMQTT(const String &message) {
+  mqttClient.poll();
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    mqttClient.beginMessage(send_topic);
+    mqttClient.print(message);
+    mqttClient.endMessage();
+    Serial.print("Published to MQTT: ");
+    Serial.println(message);
+  }
+}
 void loop() {
-
-
-  struct tempAndHumStruct tempAndHum;
   checkWifiIsConnecting();
-  getTempAndHum(&tempAndHum);
-  Serial.println("Temp");
-  Serial.print(tempAndHum.temp);
-  Serial.print("Hum");
-  Serial.print(tempAndHum.hum);
-  char buff1[20], buff2[20], buff3[60];
-  snprintf(buff1, sizeof(buff1), "Temp:   %.2f°C", tempAndHum.temp);
-  snprintf(buff2, sizeof(buff2), "Hum :   %.2f", tempAndHum.hum);
-  displayToLcd(buff1, buff2);
-  snprintf(buff3, sizeof(buff3), "{\"flag\":\"%s\",\"device\":\"%d\",\"temp\":\"%.2f\",\"hum\":\"%.2f\"}" , FLAG, DEVICE_ID ,tempAndHum.temp, tempAndHum.hum);
-  publishMessageMQTT(buff3);
+  char buff1[20], buff2[20], buff3[50];
+   if (rf95.available())
+  {
+    // Should be a message for us now   
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    
+    if (rf95.recv(buf, &len))
+    {
+      // แปลงข้อความจาก uint8_t buffer เป็น String
+    String receivedMessage = String((char*)buf);  // แปลงข้อมูลจาก uint8_t array เป็น String
+    
+    Serial.print("Got: ");
+    Serial.println(receivedMessage);  // แสดงผลข้อความที่ได้รับ
+
+    Serial.print("RSSI: ");
+    Serial.println(rf95.lastRssi(), DEC);  // แสดงค่า RSSI ของการส่ง
+
+    // ส่งข้อมูลที่ได้รับจาก LoRa ไปยัง MQTT broker
+    publishMessageMQTT(receivedMessage);
+
+    // ส่งข้อความตอบกลับ
+    uint8_t data[] = "And hello back to you";
+    rf95.send(data, sizeof(data));
+    rf95.waitPacketSent();
+    Serial.println("Sent a reply");
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
 }
